d84c1ec62e60c63a5c0d7b1d7a834790c4f90595
mcvzo <mcvzo@iscte-iul.pt>
2019-11-08 17:05:48 +0000

    inacabado

--- ES/src/Resultados.java
+++ ES/src/Resultados.java
@@ -4,12 +4,14 @@ public class Resultados {
 	private Regra regra;
 	private boolean resultado;
 	
-	public Resultados() {
-		
+	public Resultados(Regra regra) {
+		this.regra = regra;
 	}
 
-	public boolean avaliarRegras(Regra) {
-		
+	public boolean avaliarRegras(Regra regra, int locValue, int cycloValue) {
+		if(regra.getDefeito() && regra.getX()) {
+			
+		}
 	}
 	
 }
