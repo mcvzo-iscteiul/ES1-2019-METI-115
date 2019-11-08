252541a42eebc5de862cb20ccbff80fc73e2d018
catarina <acdro@iscte-iul.pt>
2019-11-08 17:28:54 +0000

    tent1

---
+++ ES/src/QualidadeFerramentas.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class QualidadeFerramentas {
+	public int DCIcount;
+	public int DIIcount;
+	public int ADCIcount;
+	public int ADIIcount;
+	
+	public static void main(String[] args) throws IOException {
+
+		ArrayList<Dados> vetor = new ArrayList<>();
+		Workbook workbook = WorkbookFactory.create(new File("../ES/src/Long-Method.xlsx"));
+		Sheet sheet = workbook.getSheetAt(0);
+
+		Iterator<Row> rowIterator = sheet.rowIterator();
+		
+		Row row = rowIterator.next();
+		while(rowIterator.hasNext()) {
+
+			row = rowIterator.next();
+			Iterator<Cell> cellIterator = row.cellIterator();
+			Dados dados = new Dados(1,2);
+
+			while(cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
+
+				if(cell.getColumnIndex() == 4) {
+					
+					dados.setX((cell.getNumericCellValue()));
+					vetor.add(dados);
+					
+				}
+				else if(cell.getColumnIndex()==5) {
+					dados.setY(cell.getNumericCellValue());
+					vetor.add(dados);
+				}
+			}
+		}
+		
+		for(Dados a: vetor) {
+			System.out.println(a.getX() + "," + a.getY());
+		}
+	}
+
+}
+
