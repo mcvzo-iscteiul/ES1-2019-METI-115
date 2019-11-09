8100472d9234100450b5cf05f8f25d98f339f796
mcvzo <mcvzo@iscte-iul.pt>
2019-11-09 18:42:29 +0000

    inacabado

--- ES/src/Regra.java
+++ ES/src/Regra.java
@@ -2,13 +2,9 @@
 public class Regra {
 	
 	private Defeito defeito;
-	private Valor val1;
-	private Valor val2;
 	
-	public Regra(Defeito defeito, Valor val1, Valor val2) {
+	public Regra(Defeito defeito) {
 		this.defeito = defeito;
-		this.val1 = val1;
-		this.val2 = val2;
 	}
 
 	public Defeito getDefeito() {
@@ -17,22 +13,6 @@ public class Regra {
 	
 	public void setDefeito(Defeito defeito) {
 		this.defeito = defeito;
-	}
-	
-	public Valor getVal1() {
-		return val1;
-	}
-	
-	public void setVal1(Valor val1) {
-		this.val1 = val1;
-	}
-	
-	public Valor getVal2() {
-		return val2;
-	}
-	
-	public void setVal2(Valor val2) {
-		this.val2 = val2;
 	}
 	
 }
