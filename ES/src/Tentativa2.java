f4fa687f0f608c9d6d1e77d063f6c9cb1be78dc4
maria <mjoop@iscte-iul.pt>
2019-11-09 16:58:27 +0000

    ALteracao Classe Tentativa2

--- ES/src/Tentativa2.java
+++ ES/src/Tentativa2.java
@@ -1,4 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class Tentativa2 {
-
+	
+	private Workbook excel;
+	ArrayList<Dados> vec = new ArrayList<>();
+	Sheet sheet;
+	Row row;
+	
+	
+	public void getDados() throws EncryptedDocumentException, IOException {
+		excel = WorkbookFactory.create(new File("../ES/src/Long-Method.xlsx"));
+		
+	}
 }
+
