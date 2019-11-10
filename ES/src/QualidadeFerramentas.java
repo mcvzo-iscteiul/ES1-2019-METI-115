00ac13ed7a2077a19c49210f23816f0578e22f33
catarina <acdro@iscte-iul.pt>
2019-11-10 17:54:20 +0000

    tentfinal

--- ES/src/QualidadeFerramentas.java
+++ ES/src/QualidadeFerramentas.java
@@ -5,26 +5,33 @@ public class QualidadeFerramentas {
 	public static int DIIcount=0;
 	public static int ADCIcount=0;
 	public static int ADIIcount=0;
-	public static ArrayList<Boolean> resultado;
+	public static ArrayList<Boolean> resultadoIsLongMeth;
+	public static ArrayList<Boolean> resultadoPMD;
+	public static ArrayList<Boolean> resultadoPlasma;
+
 
 	public static void PMD_Quality(){
-		if (resultado.get(0) == true) {
-			if (resultado.get(1) == true)
-				DCIcount++;
-			else ADIIcount++;
+		for (int i = 0; i < resultadoIsLongMeth.size(); i++) {
+			if (resultadoIsLongMeth.get(i) == true) {
+				if (resultadoPMD.get(i) == true)
+					DCIcount++;
+				else ADIIcount++;
+			}
+			System.out.println("DCI = " + DCIcount);		
+			System.out.println("ADII = " + ADIIcount);
 		}
-		System.out.println("DCI = " + DCIcount);		
-		System.out.println("ADII = " + ADIIcount);
 	}
 
 	public static void iPlasma_Quality(){
-		if (resultado.get(0) == false) {
-			if (resultado.get(1) == true)
-				DIIcount++;
-			else ADCIcount++;
+		for (int i = 0; i < resultadoIsLongMeth.size(); i++) {		
+			if (resultadoIsLongMeth.get(i) == false) {
+				if (resultadoPlasma.get(i) == true)
+					DIIcount++;
+				else ADCIcount++;
+			}	
+			System.out.println("DII = " + DIIcount);
+			System.out.println("ADCI = " + ADCIcount);
 		}
-		System.out.println("DII = " + DIIcount);
-		System.out.println("ADCI = " + ADCIcount);
 	}
 }
 
