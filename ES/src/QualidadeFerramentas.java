e093a3ab2ae79fbae76e7446654093c75ad1b3fa
catarina <acdro@iscte-iul.pt>
2019-11-13 23:23:09 +0000

    ten10

--- ES/src/QualidadeFerramentas.java
+++ ES/src/QualidadeFerramentas.java
@@ -1,57 +1,78 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.poi.EncryptedDocumentException;
+
 public class QualidadeFerramentas {
-	public static int DCIcount=0;
-	public static int DIIcount=0;
-	public static int ADCIcount=0;
-	public static int ADIIcount=0;
-	public static ArrayList<Metodos> arrayMariana;
-	public static ArrayList<Boolean> resultadoIsLongMeth;
-	public static ArrayList<Boolean> resultadoPMD;
-	public static ArrayList<Boolean> resultadoPlasma;
+	private int DCIcount=0;
+	private int DIIcount=0;
+	private int ADCIcount=0;
+	private int ADIIcount=0;
+	//private ArrayList<Metodo> arrayMariana = new TratamentoDosDadosExcel().guardarDadosMatriz();
+	//private ArrayList<Boolean> resultadoIsLongMeth;
+	private ArrayList<Metodo> resultadoPMD;
+	private ArrayList<Metodo> resultadoPlasma;
 
 
-	public static void ObtencaoILM(){
-		for (int i = 0; i < arrayMariana; i=i+6) {		
-			resultadoIsLongMeth.add(arrayMariana.get(i));
-		}
-	}
-	
-	public static void ObtencaoPlasma(){
-		for (int i = 0; i < arrayMariana; i=i+7) {		
-			resultadoIsLongMeth.add(arrayMariana.get(i));
-		}
-	}
-	
-	public static void ObtencaoPMD(){
-		for (int i = 0; i < arrayMariana; i=i+8) {		
-			resultadoIsLongMeth.add(arrayMariana.get(i));
-		}
-	}
 
-
-	public static void PMD_Quality(){
-		for (int i = 0; i < resultadoIsLongMeth.size(); i++) {
-			if (resultadoIsLongMeth.get(i) == true) {
-				if (resultadoPMD.get(i) == true)
-					DCIcount++;
-				else ADIIcount++;
-			}
-			System.out.println("DCI = " + DCIcount);		
-			System.out.println("ADII = " + ADIIcount);
+	@SuppressWarnings("null")
+	private static void ObtencaoILM(){	
+		ArrayList<Metodo> arrayMariana = new TratamentoDosDadosExcel().guardarDadosMatriz();
+		ArrayList<Boolean> resultadoIsLongMeth = null;
+		System.out.println("indice= " + arrayMariana.get(6).getIs_long_method());
+		System.out.println("indice= " + arrayMariana.get(18).getIs_long_method());
+		for (int i = 0; i < arrayMariana.size(); i=i+6) {
+			boolean item = arrayMariana.get(i).getIs_long_method();
+			System.out.println(item);
+			System.out.println(resultadoIsLongMeth);
+			resultadoIsLongMeth.add(item);
+			System.out.println(resultadoIsLongMeth);
 		}
 	}
 
-	public static void iPlasma_Quality(){
-		for (int i = 0; i < resultadoIsLongMeth.size(); i++) {		
-			if (resultadoIsLongMeth.get(i) == false) {
-				if (resultadoPlasma.get(i) == true)
-					DIIcount++;
-				else ADCIcount++;
-			}	
-			System.out.println("DII = " + DIIcount);
-			System.out.println("ADCI = " + ADCIcount);
-		}
+	//	public void ObtencaoPlasma(){
+	//		for (int i = 0; i < arrayMariana.size(); i=i+7) {		
+	//			resultadoIsLongMeth.add(arrayMariana.get(i));
+	//			//System.out.println(resultadoPlasma);
+	//		}
+	//	}
+
+	//	public void ObtencaoPMD(){
+	//		for (int i = 0; i < arrayMariana.size(); i=i+8) {		
+	//			resultadoIsLongMeth.add(arrayMariana.get(i));
+	//			//System.out.println(resultadoPMD);
+	//		}
+	//	}
+
+
+	//	public static void PMD_Quality(){
+	//		for (int i = 0; i < resultadoIsLongMeth.size(); i++) {
+	//			if (resultadoIsLongMeth.get(i) == true) {
+	//				if (resultadoPMD.get(i) == true)
+	//					DCIcount++;
+	//				else ADIIcount++;
+	//			}
+	//			System.out.println("DCI = " + DCIcount);		
+	//			System.out.println("ADII = " + ADIIcount);
+	//		}
+	//	}
+	//
+	//	public static void iPlasma_Quality(){
+	//		for (int i = 0; i < resultadoIsLongMeth.size(); i++) {		
+	//			if (resultadoIsLongMeth.get(i) == false) {
+	//				if (resultadoPlasma.get(i) == true)
+	//					DIIcount++;
+	//				else ADCIcount++;
+	//			}	
+	//			System.out.println("DII = " + DIIcount);
+	//			System.out.println("ADCI = " + ADCIcount);
+	//		}
+	//	}
+
+	public static void main(String [] args) throws EncryptedDocumentException, IOException {
+		ObtencaoILM();
+		//ObtencaoPlasma();
+		//ObtencaoPMD();
 	}
 }
 
---
+++ ES/src/TratamentoDosDadosExcel.java
@@ -0,0 +1,98 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class TratamentoDosDadosExcel {
+
+
+	
+
+	static ArrayList<Metodo> matrizExcel = new ArrayList<>();
+	Workbook excel;
+	Sheet folha;
+
+	public TratamentoDosDadosExcel(){
+		try {
+			this.excel =  WorkbookFactory.create(new File("../ES/src/Long-Method.xlsx"));
+		} catch (EncryptedDocumentException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.folha = excel.getSheetAt(0);
+	}
+
+	public ArrayList<Metodo> guardarDadosMatriz() {
+		Iterator <Row> linhaIterator = folha.rowIterator();
+		Row linha = linhaIterator.next();
+		while(linhaIterator.hasNext()) {
+			Metodo metodo = new Metodo("a",1,2,3,4,true,true,true);
+			linha = linhaIterator.next();
+			Iterator <Cell> celulaIterator = linha.cellIterator();
+			while(celulaIterator.hasNext()) {
+				Cell celula = celulaIterator.next();	
+				guardarDadosNoVetor(celula, metodo);
+
+			}this.matrizExcel.add(metodo);
+		}for(Metodo a: matrizExcel) {
+			//System.out.println(a.getMetodo() + ", " + a.getLOC() + ", " + a.getCYCLO() + ", " + a.getATFD()+ ", " + a.getLAA() + ", " + a.getIs_long_method() + ", " + a.getiPlasma() + ", " + a.getPMD() + ", " + a.getIs_feature_envy());
+		}
+		return matrizExcel;
+	}
+
+	private void guardarDadosNoVetor(Cell celula, Metodo metodo) {
+
+		if(celula.getColumnIndex() == 3) {
+			metodo.setMetodo(celula.getStringCellValue());
+		}if(celula.getColumnIndex() == 4) {
+			metodo.setLOC(celula.getNumericCellValue());
+		}if(celula.getColumnIndex() == 5) {
+			metodo.setCYCLO(celula.getNumericCellValue());
+		}if(celula.getColumnIndex() == 6) {
+			metodo.setATFD(celula.getNumericCellValue());
+		}if(celula.getColumnIndex() == 7) {
+			if(celula.getCellType().equals(CellType.NUMERIC)) {
+				metodo.setLAA(celula.getNumericCellValue());
+			}else if(celula.getCellType().equals(CellType.STRING)) {
+				metodo.setLAA(Double.parseDouble(celula.getStringCellValue()));
+			}
+		}if(celula.getColumnIndex() == 8) {
+			stringBoolean(celula);
+			metodo.setIs_long_method(celula.getBooleanCellValue());
+		}if(celula.getColumnIndex() == 9) {
+			stringBoolean(celula);
+			metodo.setiPlasma(celula.getBooleanCellValue());
+		}if(celula.getColumnIndex() == 10) {
+			stringBoolean(celula);
+			metodo.setPMD(celula.getBooleanCellValue());
+		}if(celula.getColumnIndex() == 11) {
+			stringBoolean(celula);
+			metodo.setIs_feature_envy(celula.getBooleanCellValue());				
+		} 
+	} 
+
+
+	private void stringBoolean(Cell cell) {
+		if(cell.getCellType().equals(CellType.STRING)) {
+			cell.setCellType(CellType.BOOLEAN);
+		}
+	}
+
+	public static void main(String [] args) throws EncryptedDocumentException, IOException {
+		new TratamentoDosDadosExcel().guardarDadosMatriz();
+	}
+
+
+	public ArrayList<Metodo> getMatrizExcel() {
+		return matrizExcel;
+	}
+}
