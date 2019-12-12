0f0e31f781d3b97de8a9e645aa2cad475c8c8056
mcvzo <mcvzo@iscte-iul.pt>
2019-12-12 13:01:30 +0000

    erros?

---
+++ ES/src/GUI/JanelaIPlasma.java
@@ -0,0 +1,92 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import results.QualidadeFerramentas;
+import utils.Metodo;
+
+/**
+ * @author carlosguerra
+ *
+ */
+public class JanelaIPlasma {
+	
+	private ArrayList<Metodo> arrayExcel;
+	private ArrayList<Integer> res;
+	
+	/**
+	 * @param arrayExcel
+	 * Constructor for the window, this window needs the arrayExcel to be able to display the results
+	 */
+	public JanelaIPlasma(ArrayList<Metodo> arrayExcel) {
+		super();
+		this.arrayExcel = arrayExcel;
+		makeWindow();
+	}
+	
+	/**
+	 * Makes the window, also displaying the results based on the Excel file which was given on the constructor
+	 */
+	private void makeWindow() {
+		
+		JFrame frame = new JFrame("Resultados IPlasma");
+		
+		JPanel painel = new JPanel(new BorderLayout());
+		
+		JTextArea resul = new JTextArea();
+		resul.setBounds(10, 79, 179, 339);
+		resul.setEditable(false);
+		
+		res = QualidadeFerramentas.iPlasma_Quality(arrayExcel);
+		
+		for(int i = 0; i < res.size(); i++) {
+			switch(i){
+			case(0):
+				resul.append("DCI do iPlasma =" + res.get(i) + "\n");
+				break;
+			
+			case(1):
+				resul.append("DII do iPlasma =" + res.get(i) + "\n");
+				break;
+			
+			case(2):
+				resul.append("ADII do iPlasma =" + res.get(i) + "\n");
+				break;
+				
+			case(3):
+				resul.append("ADCI do iPlasma =" + res.get(i) + "\n");
+				break;
+			}
+		}
+		
+		painel.add(resul,BorderLayout.CENTER);
+		
+		JButton ok = new JButton("Ok");
+		painel.add(ok, BorderLayout.SOUTH);
+		
+		frame.add(painel);
+		frame.pack();
+		
+		frame.setLocationRelativeTo(null);
+		
+		frame.setVisible(true);
+		
+		ok.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		
+	}
+	
+
+}
---
+++ ES/src/GUI/JanelaQuality.java
@@ -0,0 +1,96 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import results.QualidadeFerramentas;
+import utils.Estrutura;
+import utils.Metodo;
+
+/**
+ * @author carlosguerra
+ *
+ */
+public class JanelaQuality {
+	
+	private ArrayList<Metodo> arrayExcel;
+	private ArrayList<Integer> res;
+	private ArrayList<Estrutura> arrayResultados;
+	
+	/**
+	 * @param arrayExcel
+	 * @param arrayResultados
+	 * Constructor for the object, this windows needs both the Arrays to be able to display the results
+	 */
+	public JanelaQuality(ArrayList<Metodo> arrayExcel, ArrayList<Estrutura> arrayResultados) {
+		super();
+		this.arrayExcel = arrayExcel;
+		this.arrayResultados = arrayResultados;
+		makeWindow();
+	}
+	
+	/**
+	 * Creates the Window displaying the results for the quality of the rule made by the user
+	 */
+	private void makeWindow() {
+		
+		JFrame frame = new JFrame("Resultados Quality");
+		
+		JPanel painel = new JPanel(new BorderLayout());
+		
+		JTextArea resul = new JTextArea();
+		resul.setBounds(10, 79, 179, 339);
+		resul.setEditable(false);
+		
+		res = QualidadeFerramentas.Regras_Quality(arrayExcel, arrayResultados);
+		
+		for(int i = 0; i < res.size(); i++) {
+			switch(i){
+			case(0):
+				resul.append("DCI das Regras =" + res.get(i) + "\n");
+				break;
+			
+			case(1):
+				resul.append("DII das Regras =" + res.get(i) + "\n");
+				break;
+			
+			case(2):
+				resul.append("ADII das Regras =" + res.get(i) + "\n");
+				break;
+				
+			case(3):
+				resul.append("ADCI das Regras =" + res.get(i) + "\n");
+				break;
+			}
+		}
+		
+		painel.add(resul,BorderLayout.CENTER);
+		
+		JButton ok = new JButton("Ok");
+		painel.add(ok, BorderLayout.SOUTH);
+		
+		frame.add(painel);
+		frame.pack();
+		
+		frame.setLocationRelativeTo(null);
+		
+		frame.setVisible(true);
+		
+		ok.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		
+	}
+	
+
+}
